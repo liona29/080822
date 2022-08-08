b45ab2ac372b8cbb57710becae6e6eb2a1dfa8d2
liona29 <108495117+liona29@users.noreply.github.com>
2022-08-08 19:30:48 +0300

    Update fun.groovy

--- vars/fun.groovy
+++ vars/fun.groovy
@@ -1,10 +1,15 @@
-def ourDateStamp(string format){
-    def d = new Date(0)
-     def tz = TimeZone.getTimeZone('GMT')
-    if(format.toChat(0)=='M' && format.toChat(1)=='M') 
-        return d.format('MMM/dd/yyyy', tz)
-    else if(format.toChat(2)=='M' && format.toChat(3)=='M')
-        return d.format('dd/MMM/yyyy', tz)
-    else:
-        return "wrong fornat"
+import java.util.Date
+
+def ourDateStamp(String format)
+{
+    d = new Date()
+    tz = TimeZone.getTimeZone('GMT')
+    if(format.equals('ddmmyy')
+    {
+        return d.format('dd/MM/yy', tz)
+    }
+    if(format.equals('mmddyy')
+    {
+        return d.format('MM/dd/yy', tz)
+    }
 }
