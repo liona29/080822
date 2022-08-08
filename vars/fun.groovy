2014ae883ae86974e4a0579e12bf9e7b8cd4a4ee
liona29 <108495117+liona29@users.noreply.github.com>
2022-08-08 20:03:24 +0300

    Update fun.groovy

--- vars/fun.groovy
+++ vars/fun.groovy
@@ -2,13 +2,16 @@ import java.util.Date
 
 def ourDateStamp(String format)
 {
-    d = new Date()
+   println("we in the methude")
+   def d = new Date()
     if(format.equals('ddmmyy'))
     {
+        println("ddmmyy"
         return d.format('dd/MM/yy')
     }
     if(format.equals('mmddyy'))
     {
+        println("mmddyy")
         return d.format('MM/dd/yy')
     }
 }
